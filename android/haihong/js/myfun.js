6a51fa47d42ba4f765aafe7607c85846865cdf87
dybangel <176682769@qq.com>
2019-05-07 01:09:26 +0800

    bak0507

--- android/haihong/js/myfun.js
+++ android/haihong/js/myfun.js
@@ -12,6 +12,8 @@
 		success : function(res){
     	//alert(res.server);  
         Gserver=res.server;
+        Gimgserver=res.imgserver;
+        Gmainviewid=res.mainviewid;
        // alert("gserver="+Gserver);
         }
 	});
@@ -30,18 +32,20 @@ function closeme(){
  	plus.webview.close(ws);
  }
 
-
-function getjson(action,par){
+function getsendsms(action,par){
+	 var Gurl=Gimgserver+"/api/user.aspx?action=SendMobile"+par;
+    //console.log("url is ++++++++++++++:"+Gurl);
 	var actions=action;
 	var action=actions.split("|")[0];
 	var action_2=actions.split("|")[1];
     var action_3=actions.split("|")[2];
+   
     //alert('action ready');
 	 $.ajax({
       		type:"get",
-      		url:Gserver+"/api.aspx?"+par,
+      		url:Gurl,
 			async:true,
-      		data:{"action":action},
+      		//data:{"action":action},
       		dataType:"json", // 因为PHP返回数据是JSON格式，所以这里类似要用JSON
       		success:function(data){
 
@@ -82,7 +86,72 @@ function getjson(action,par){
       					//alert(e);
       					}								
       							}else{
-      								alert(action+'返回空值，增加一个procedure返回值');
+      								console.log(action+'返回空值，增加一个procedure返回值');
+      								eval(action+'(data)');  
+      								//$('.mui-spinner').removeClass('mui-spinner');
+									//$('.mui-pull-caption-refresh')[0].innerHTML='没有更多的数据了';
+      							}
+								 },
+			error: function(xhr, type, errorThrown) {
+					console.log(errorThrown);
+							 						}
+			})
+}
+
+function getjson(action,par){
+	var actions=action;
+	var action=actions.split("|")[0];
+	var action_2=actions.split("|")[1];
+    var action_3=actions.split("|")[2];
+    console.log("appid is:"+plus.push.getClientInfo().appid);
+    //pPyZWvH3Fa6PXba10aJ009 hbulider
+    //alert('action ready');
+	 $.ajax({
+      		type:"get",
+      		url:Gserver+"/api.aspx?"+par,//+"&appid="+plus.push.getClientInfo().appid 
+			async:true, 
+      		data:{"appid":plus.push.getClientInfo().appid,"action":action}, 
+      		dataType:"json", // 因为PHP返回数据是JSON格式，所以这里类似要用JSON
+      		success:function(data){ 
+
+      			if(data!="null"){	
+      				try{		
+
+      					
+      						if (typeof(action_2)=="undefined") {
+      						//alert('test='+data);
+      					//	eval(action+'(data)');
+							//						alert('不用模板机制');
+							}else{
+								var tpid=$('#'+action+'_template')[0];
+								if(typeof(tpid)=="undefined"){
+									alert("模板："+action+"_template 不存在，请设置");
+									return;
+								}
+								var conid=$('#'+action+'_container')[0];
+								if(typeof(conid)=="undefined"){
+									alert("容器："+action+"_container 不存在，请设置");
+									return;
+								}
+								
+								var template = $('#'+action+'_template')[0];
+								var  dot = doT.template(template.innerHTML);
+								if(action_3=="append"){
+									$('#'+action+'_container')[0].innerHTML+=dot(data);
+								}else{
+									$('#'+action+'_container')[0].innerHTML=dot(data);
+								}
+								//alert('需要找模板绑定');
+							}
+						//再执行同action私有实现函数以满足一些特殊要求
+						//alert("eval run.."+action);
+      					eval(action+'(data)');  		
+      					}catch(e){
+      					//TODO handle the exception
+      					//alert(e);
+      					}								
+      							}else{
+      								console.log(action+'返回空值，增加一个procedure返回值');
       								eval(action+'(data)');  
       								//$('.mui-spinner').removeClass('mui-spinner');
 									//$('.mui-pull-caption-refresh')[0].innerHTML='没有更多的数据了';
@@ -139,9 +208,9 @@ function getvalue_plus(){
 		eval(cmd);
 		}
 	Gvalue_plus=cmds;
-	alert("This is fun.js info:"+Gvalue_plus);
+	//alert("This is fun.js info:"+Gvalue_plus);
 }
-
+ 
 //全文替换
 function replaceall(str,src,dst){
 	var reg1=new RegExp(src,"g"); //创建正则RegExp对象   
@@ -198,3 +267,183 @@ function replaceall(str,src,dst){
 							  return fmt;   
 							}
 							
+	var wgtWaiting = null;  
+
+function downloadWgt(strInstall,functions) {  
+		//console.log("myfun callback is:"+callback);
+	var action=functions;
+	//eval(action+'()');
+    
+    wgtWaiting = plus.nativeUI.showWaiting("开始下载");
+    var wgtUrl = strInstall; 
+    var wgtOption = { filename: "_doc/update/", retry: 1 };  
+    var task = plus.downloader.createDownload(wgtUrl, wgtOption, function (download, status) {  
+        if (status == 200) {  
+            wgtWaiting.setTitle("开始安装");  
+            if("update"==action){
+    		updateWgt(download.filename);  
+    		}else{
+    		installApp(download.filename,action);  	
+    		}
+            
+        } else {  
+            mui.alert("安装包下载失败！");  
+            wgtWaiting.close();  
+        }  
+    });  
+    task.addEventListener("statechanged", function (download, status) {  
+        switch (download.state) {  
+            case 2:  
+                wgtWaiting.setTitle("已连接到服务器");  
+                break;  
+            case 3:  
+                var percent = download.downloadedSize / download.totalSize * 100;  
+                wgtWaiting.setTitle("已下载 " + parseInt(percent) + "%");  
+                break;  
+            case 4:  
+                wgtWaiting.setTitle("下载完成");  
+                break;  
+        }  
+    });  
+    task.start();  
+};  
+
+function installApp(wgtpath,functions) {  
+// var action=functions;	
+    plus.runtime.install(wgtpath, {}, function (wgtinfo) {     
+        //eval(action+'()');
+        wgtWaiting.close();        
+    }, function (error) {  
+        wgtWaiting.close();  
+      //  mui.alert("应用更新失败！\n" + error.message);  
+    });  
+}; 
+
+function updateWgt(wgtpath) {  
+// var action=functions;	
+    plus.runtime.install(wgtpath, {}, function (wgtinfo) {     
+         mui.alert("更新完成，须重启应用！", function () {  
+          plus.runtime.restart();  
+      });  
+        wgtWaiting.close();        
+    }, function (error) {  
+        wgtWaiting.close();  
+        mui.alert("应用更新失败！\n" + error.message);  
+    });  
+}; 
+function checkapp(packname){
+//var packageName = 'com.tencent.mm';
+//var main = plus.android.runtimeMainActivity();  
+//var packageManager = main.getPackageManager();  
+//var PackageManager = plus.android.importClass(packageManager)  
+//var packageInfo = packageManager.getPackageInfo(packageName,PackageManager.GET_ACTIVITIES);  
+//if(packageInfo ){  
+//  alert('已安装' + packageName + '')  
+//}  
+if(plus.runtime.isApplicationExist({pname:packname})){
+		return 1;
+	}else{
+		return 0;
+	}
+
+}
+
+function openapp(packname,function1,function2){
+	//console.log("this is openapp diaoyong callback");
+	//eval(function1+'()');
+	plus.runtime.launchApplication(		
+		{pname:packname}, 
+		function ( e ) {
+				alert( "failed: " + e.message );
+				//eval(function2+'()');
+})
+	
+}
+//获取cpu信息
+function getCpuInfo() {
+ var cpuInfo = '/proc/cpuinfo';
+ var temp = '',
+  cpuHardware;
+ var fileReader = plus.android.importClass("java.io.FileReader");
+ var bufferedReader = plus.android.importClass("java.io.BufferedReader");
+ var FileReader = new fileReader(cpuInfo);
+ var BufferedReader = new bufferedReader(FileReader, 8192);
+ while ((temp = BufferedReader.readLine()) != null) {
+  if (-1 != temp.indexOf('Hardware')) {
+   cpuHardware = temp.substr(parseInt(temp.indexOf(":")) + 1);
+  }
+ }
+ return cpuHardware;
+}
+
+//本地数据存储
+function setItemFun(key,value) {
+		plus.storage.setItem( key, value );		
+		//mui.toast( "数据存储成功，存储了"+key+" value为；"+value );
+}
+//循环打印obj
+function printobj(obj){
+for(var key in obj){
+  console.log('key='+key+'  value='+obj[key]);
+}
+	
+}
+//function copytoclip(mystr){
+//	var tempstr=mystr;
+//  var Context = plus.android.importClass("android.content.Context");
+//  var main = plus.android.runtimeMainActivity();
+//  var clip = main.getSystemService(Context.CLIPBOARD_SERVICE);
+//  plus.android.invoke(clip,"setText",tempstr);
+//}
+//复制内容到剪切板
+function copy_fun(copy){//参数copy是要复制的文本内容
+	mui.plusReady(function(){
+		//判断是安卓还是ios
+		if(mui.os.ios){
+			//ios
+			var UIPasteboard = plus.ios.importClass("UIPasteboard");
+		    var generalPasteboard = UIPasteboard.generalPasteboard();
+		    //设置/获取文本内容:
+		    generalPasteboard.plusCallMethod({
+		        setValue:copy,
+		        forPasteboardType: "public.utf8-plain-text"
+		    });
+		    generalPasteboard.plusCallMethod({
+		        valueForPasteboardType: "public.utf8-plain-text"
+		    });
+		    mui.toast("已成功复制到剪贴板");
+		}else{
+			//安卓
+			var context = plus.android.importClass("android.content.Context");
+			var main = plus.android.runtimeMainActivity();
+			var clip = main.getSystemService(context.CLIPBOARD_SERVICE);
+			plus.android.invoke(clip,"setText",copy);
+			mui.toast("已成功复制到剪贴板");
+		}
+	});
+}
+
+//function showmessage(title,content,nofun,yesfun){
+//		var btnArray = ['否', '是'];
+//  var message = "<h4>"+content+"</h4>";
+// 	//console.log("yesfun  is:"+yesfun);
+// 	Gyesfun=yesfun;
+// 	Gnofun=nofun;
+//  //$('#alert-bg').css('display','block');
+//  mui.confirm(message, title, btnArray, function(e,yesfun,nofun) {
+//  	console.log("dddyesfun  is:"+Gyesfun);
+//  	//printobj(e);
+//      if (e.index == 1) {
+//      	eval(Gyesfun+'();');
+//        //  alert("shi"); 
+// 
+//      } else {
+//      	eval(Gnofun+'();');
+//      	//alert("0");
+//
+//      }
+//  },'div');
+//}
+
+
+
--- api/api/api.aspx.cs
+++ api/api/api.aspx.cs
@@ -13,65 +13,263 @@ public partial class api : System.Web.UI.Page
     {
         string getstr = HttpContext.Current.Request.Url.Query;
          getstr = getstr.Replace("?", "");
+        getstr = getstr.Replace("&appid=pPyZWvH3Fa6PXba10aJ009", "");//hbulider的
+        getstr = getstr.Replace("&appid=FWEFASDFSFA","");//我的
         int start = getstr.LastIndexOf("&");
         int stop = getstr.Length - start;
          string where = getstr.Substring(0,start);
        // Response.Write("<script>console.log('where=" + where + "')</script>");
-        string procedure = getstr.Substring(start,stop).Replace("&action=","").ToString();
+        string procedure = getstr.Substring(start,stop).Replace("&action=", "").ToString();
 
         string action = Request["action"];
         string mysql = "";
+        //获取已经添加的设备数量
         if ("getcount" == action)
         {
             mysql = "SELECT COUNT(*) AS wcount FROM Fooke_Udid";
         }
+        //通过UDID添加白名单
         else if ("adddata_fromudid" == action)
         {
             string datas = Request["datas"];
+
+            //判断是否有重复记录，如果有则退出
+            mysql = "SELECT COUNT(udid) AS numcount FROM Fooke_Udid WHERE (udid = '" + datas + "')";
+            DataTable dt = executesql(mysql);
+            if (0 < Convert.ToInt32(dt.Rows[0]["numcount"]))
+            {
+                returnjson("status", "UDID已经存在");
+                Response.End();
+            }
+
+
             mysql = "INSERT INTO Fooke_Udid(udid)VALUES('" + datas + "')";
+            executesql(mysql);
+            returnjson("status", "ok");
+            Response.End();
         }
+        //通过userid添加白名单
         else if ("adddata_fromuserid" == action)
         {
             string datas = Request["datas"];
+            //判断userid是否存在
+            mysql = "select count(udid) as numcount from Fooke_User where (userid='" + datas + "')";
+            DataTable dt = executesql(mysql);
+            if (0 == Convert.ToInt32(dt.Rows[0]["numcount"]))
+            {
+                returnjson("status", "邀请码存在");
+                Response.End();
+            }
+
+            //判断userid对应的udid是否有重复
+
             mysql = "INSERT INTO Fooke_Udid (udid) SELECT DeviceIdentifier FROM Fooke_User WHERE(UserID = '" + datas + "')";
+            executesql(mysql);
+            returnjson("status", "ok");
+            Response.End();
         }
-        else if ("devicechange" == action) {
+        //更换设备
+        else if ("devicechange" == action)
+        {
             string olduserid = Request["olduserid"];
             string newudid = Request["newudid"];
-            mysql = "select userid,strTokey from fooke_user where userid='"+ olduserid + "'";
+            mysql = "select userid,strTokey from fooke_user where userid='" + olduserid + "'";
             DataTable dt = new DataTable();
-            dt=executesql(mysql);          
-            string oldstrtokey=dt.Rows[0]["strtokey"].ToString();
+            dt = executesql(mysql);
+            string oldstrtokey = dt.Rows[0]["strtokey"].ToString();
             string jsonString = string.Empty;
             dt.Clear();
 
             mysql = "select userid,strTokey from fooke_user where DeviceIdentifier='" + newudid + "'";
-          
+
             dt.Clear();
             dt = executesql(mysql);
-            if (0 == dt.Rows.Count) {
+            if (0 == dt.Rows.Count)
+            {
                 returnjson("status", "该UDID不存在，1、请确认新设备已经安装海虹 2、确认在新设备上联网打开过海虹 3、确认UDID粘贴无误");
                 Response.End();
             }
-           
 
-            string newuserid= dt.Rows[0]["userid"].ToString();
+
+            string newuserid = dt.Rows[0]["userid"].ToString();
             string newstrtokey = dt.Rows[0]["strtokey"].ToString();
 
-            mysql = "update fooke_user set strtokey='" + newstrtokey + "' where userid='"+olduserid+"'";
+            mysql = "update fooke_user set strtokey='" + newstrtokey + "' where userid='" + olduserid + "'";
             executesql(mysql);
-          
+
 
             mysql = "update fooke_user set strtokey='" + oldstrtokey + "' where userid='" + newuserid + "'";
             executesql(mysql);
-            returnjson("status","ok");
+            returnjson("status", "ok");
             dt.Dispose();
             Response.End();
         }
-        else
+        //自定义sql语句
+        else if (action.IndexOf("sql_") >= 0)
         {
-           mysql = "exec [" + procedure + "] " + HttpUtility.UrlDecode(where);
-           
+
+            mysql = Request["sql"];
+            //  returnjson("status", mysql);
+            //  Response.End();
+            // returnjson("status", mysql);
+            string jsonString = string.Empty;
+
+            jsonString = JsonConvert.SerializeObject(executesql(mysql));
+            Response.Write(jsonString);
+
+            //  returnjson("status", "this is sql");
+            Response.End();
+            // executesql();
+        }
+        else if ("appupdate" == action)
+        {
+            Hashtable hash1 = new Hashtable();
+            hash1.Add("status", "1.2.4");
+            hash1.Add("mode", "apk");
+            List<Hashtable> L_hash = new List<Hashtable>();
+            L_hash.Add(hash1);
+            Response.Write(JsonConvert.SerializeObject(L_hash));
+            //returnjson("status", "1.1.6");
+            Response.End();
+        }
+        else if ("ticheng" == action)
+        {
+            string amount = Request["amount"];
+            string userid = Request["userid"];
+            string username = Request["username"];
+            var sql = "exec [Stored_FindUserParent] @NodeID=N'" + userid + "'";
+            DataTable dt = new DataTable();
+            dt = executesql(sql);
+            string tuserid = "";
+            string tparentid = "";
+            string tnickname = "";
+            string tNodeLevel = "";
+            string tempstr = "";
+            float ticheng = (float)(Convert.ToDouble(amount) * 0.02f);
+            ticheng=(float)(Math.Round(Convert.ToDecimal(ticheng), 2, MidpointRounding.AwayFromZero));
+            //returnjson("status", dt.Rows.Count.ToString());
+            //    Response.End();
+            for (int i = 1; i <= dt.Rows.Count - 1; i++)
+            {
+                tuserid = dt.Rows[i]["userid"].ToString();
+                tparentid = dt.Rows[i]["parentid"].ToString();
+                tnickname = dt.Rows[i]["nickname"].ToString();
+                tNodeLevel = dt.Rows[i]["NodeLevel"].ToString();
+                // tempstr = tempstr + "userid:" + tuserid + " parentid:" + tparentid + " nickname:" + tnickname + " NodeLevel:" + tNodeLevel;
+                sql = "exec [Stored_SaveAmount] @strKey=N'" + getRandrom(14) + GetTimeStamp() + "',@UserID=N'" + tuserid + "',@Nickname=N'" + tnickname + "',@FormID=N'" + userid + "',@Formname=N'" + username + "',@Affairs=N'1',@Mode=N'任务提成',@Amount=N'" + ticheng.ToString() + "',@Remark=N'下级好友完成任务获得奖励提成" + ticheng.ToString() + "元'";
+                executesql(sql);
+            }
+
+            returnjson("status", "ok");
+            Response.End();
+
+        }
+        else if ("jicha" == action)
+        {
+            
+            string amount = Request["amount"];
+            string userid = Request["userid"];
+            string username = Request["username"];
+            var sql = "exec [Stored_FindUserParent] @NodeID=N'" + userid + "'";
+
+            DataTable dt = new DataTable();
+            //得到师傅结果集
+            dt = executesql(sql);
+          
+              dt.Columns.Add("umbrella", typeof(string));
+              dt.Columns.Add("jichaticheng", typeof(string));
+            dt.Columns.Add("jichatichengamount", typeof(string));
+            //循环计算出每一个师傅伞下成员数量
+            DataTable dtdt = new DataTable();
+            //循环前声明提成能量槽为4%，级别为0.00
+            float GC = 0.04f;
+            float GL = 0.00f;
+            for (int i = 1; i <= dt.Rows.Count-1; i++) {
+                string dtuserid = dt.Rows[i]["userid"].ToString();
+                string dtsql = " WITH CTE AS (";
+                dtsql += " SELECT UserID, ParentID FROM Fooke_User WHERE(UserID = "+dtuserid+")";
+                dtsql += " UNION ALL";
+                dtsql += " SELECT a.UserID, a.ParentID FROM Fooke_User AS a";
+                dtsql += " INNER JOIN CTE AS b ON b.userID = a.ParentID)";
+
+                dtsql += " SELECT count(*)as umbrellacount FROM Fooke_User AS a";
+                dtsql += " INNER JOIN CTE AS t ON a.UserID = t.UserID";
+               dtdt=executesql(dtsql);
+                dt.Rows[i]["umbrella"] = dtdt.Rows[0]["umbrellacount"];
+                //根据这个用户伞下的会员数计算它应该得到的提成比例
+                float levelpercent=getlevelpercent(Convert.ToInt32(dt.Rows[i]["umbrella"]));
+                //如果GC-levelpercent >=0,并且levelpercent〉GL说明还可以提成
+                decimal cha = Convert.ToDecimal(GC) - Convert.ToDecimal(levelpercent);
+                // decimal level = Convert.ToDecimal() - Convert.ToDecimal();
+                if (cha >= 0 && Convert.ToDecimal(levelpercent) > Convert.ToDecimal(GL))
+                {
+                    decimal jichaticheng = Convert.ToDecimal(amount) * Convert.ToDecimal(levelpercent);
+                     jichaticheng=(Math.Round(Convert.ToDecimal(jichaticheng), 2, MidpointRounding.AwayFromZero));
+                    dt.Rows[i]["jichaticheng"] = levelpercent.ToString();
+                    dt.Rows[i]["jichatichengamount"] = jichaticheng;
+                    GC = (float)(cha);
+                    GL = levelpercent;
+                    //更新GC 更新GL
+                }
+                else {
+                    dt.Rows[i]["jichaticheng"] = "0.00";
+                }
+        
+                dtdt.Clear();
+            }
+
+
+            string jsonString = string.Empty;
+            jsonString = JsonConvert.SerializeObject(dt);
+            Response.Write(jsonString);
+            Response.End();
+        }
+        else if ("checkreg" == action) {
+            string sn = Request["sn"];
+            string uuid = Request["uuid"];
+           // returnjson("status", "激活码异常(000)");
+            //Response.End();
+            //看sn存在否
+        string sql = "SELECT TOP (1) fid, fsn, fuuid FROM Fooke_licence WHERE(fsn = '"+sn+"')";
+            DataTable dt = new DataTable();
+            dt = executesql(sql);
+            if (dt.Rows.Count == 0)
+            {
+                returnjson("status", "激活码异常(000)");
+                Response.End();
+            } 
+            else {
+                //看激活码是否被使用过
+                if (dt.Rows[0]["fuuid"] == "NULL" || dt.Rows[0]["fuuid"] == "")
+                {
+                    //激活码没有被使用过
+                    sql = "update Fooke_licence set fuuid=" + uuid + " WHERE(fsn = '" + sn + "')";
+                    executesql(sql);
+                    returnjson("status", "激活成功");
+                    Response.End();
+                }
+                else {
+                    //激活码被使用过
+                    if (dt.Rows[0]["fuuid"].ToString() == uuid)
+                    {
+                        //说明是本人
+                        returnjson("status", "序列号与UUID匹配");
+                        Response.End();
+                    }
+                    else {
+                        //说明不是本人
+                        returnjson("status", "不是本人" + "dt uuid is:" + dt.Rows[0]["fuuid"].ToString() + "post uuid is:" + uuid);
+                        Response.End();
+                    }
+
+                }
+
+            }   
+        }
+        else //执行存储过程
+        {
+            mysql = "exec [" + procedure + "] " + HttpUtility.UrlDecode(where);
+
         }
         SqlConnection MyConnection = new SqlConnection("data source=localhost;initial catalog=HaiHong;password=hh123456;persist s" +
 "ecurity info=True;user id=sa;workstation id=BAIHAO;packet size=4096");
@@ -85,9 +283,20 @@ public partial class api : System.Web.UI.Page
             DataTable dt = new DataTable();
             ada.Fill(dt);
             string jsonString = string.Empty;
-            jsonString = JsonConvert.SerializeObject(dt);
+            if (0==dt.Rows.Count) {
+               
+                returnjson("status","NULL");
+                
+            }
+            else {
+             
+                jsonString = JsonConvert.SerializeObject(dt);
+                Response.Write(jsonString);
+            }
+            
+            
 
-            Response.Write(jsonString);
+           
             MyConnection.Close();
             dt.Dispose();
             ds.Dispose();
@@ -95,13 +304,40 @@ public partial class api : System.Web.UI.Page
         }
         catch
         {
-            returnjson("status", "error");
+            returnjson("status", "error111");
         }
         finally {
         
         }
 
     }
+    //随机数
+ protected string getRandrom(int num)
+    {
+
+        string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+        Random randrom = new Random((int)DateTime.Now.Ticks);
+
+        string str = "";
+        for (int i = 0; i < num; i++)
+        {
+            str += chars[randrom.Next(chars.Length)];
+        }
+
+        return str;
+
+}
+
+
+    public static string GetTimeStamp()
+    {
+        TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        return Convert.ToInt64(ts.TotalSeconds).ToString();
+    }
+
+    //向前台打印json
     protected void returnjson(string tag,string str) {
         Hashtable hash1 = new Hashtable();
         hash1.Add(tag, str);
@@ -109,6 +345,27 @@ public partial class api : System.Web.UI.Page
         L_hash.Add(hash1);
         Response.Write(JsonConvert.SerializeObject(L_hash));
     }
+    //根据伞下人数判断级别
+    protected float getlevelpercent(int umbrellacount) {
+        float levelpercent = 0f;
+        if (umbrellacount >= 200 && umbrellacount < 500)
+        {
+            return 0.01f;//铜
+        }
+        else if (umbrellacount >= 500 && umbrellacount < 1000) {
+            return 0.02f;//银
+        }
+        else if (umbrellacount >= 1000 && umbrellacount < 2000)
+        {
+            return 0.03f;//金
+        }
+        else if (umbrellacount >= 2000 )
+        {
+            return 0.04f;//钻石
+        }
+        return levelpercent;
+    }
+    //执行sql返回dt
     protected DataTable executesql(string sql,string tag="") {
 
         SqlConnection MyConnectiondc = new SqlConnection("data source=localhost;initial catalog=HaiHong;password=hh123456;persist s" +
@@ -140,6 +397,10 @@ public partial class api : System.Web.UI.Page
 
 
     }
+
+
+
+
 }
 
 //  Response.Write("<script>console.log('kv=" + getstr + "')</script>");
